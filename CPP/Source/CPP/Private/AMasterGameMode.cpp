9586cd43b3b6895260d1ca84a74021b31c4bdb90
S0l4k <lukasz.drobik6@gmail.com>
2025-12-09 18:28:26 +0100

    5 dead XD

--- CPP/Source/CPP/Private/AMasterGameMode.cpp
+++
@@ -1,9 +0,0 @@
-#include "AMasterGameMode.h"
-#include "AABasePlayerCharacter.h"
-#include "AABasePlayerController.h"
-
-AAMasterGameMode::AAMasterGameMode()
-{
-    DefaultPawnClass = AABasePlayerCharacter::StaticClass();
-    PlayerControllerClass = AABasePlayerController::StaticClass();
-}
